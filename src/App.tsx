d38ecea34917894e6f0dae127ce341251b83ca32
Jackson Tong <tongtoan2704@gmail.com>
2023-11-29 09:48:07 +0700

    compare pdf

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,36 @@ function App() {
     WebViewer(
       {
         path: "/webviewer/lib",
-        licenseKey: "your_license_key", // sign up to get a free trial key at https://dev.apryse.com
+        licenseKey: "your_license_key", // sign up to get a free trial key at https://dev.apryse.com,
+        fullAPI: true,
       },
       viewer.current!
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      const { UI, Core } = instance;
+      const { Annotations } = Core;
+      const { Color } = Annotations;
+      UI.enableFeatures([UI.Feature.MultiViewerMode]);
+
+      UI.addEventListener(UI.Events.MULTI_VIEWER_READY, () => {
+        UI.enableMultiViewerSync();
+
+        const [documentViewer1, documentViewer2] = Core.getDocumentViewers();
+        const startCompare = async () => {
+          const shouldCompare =
+            documentViewer1.getDocument() && documentViewer2.getDocument();
+          if (shouldCompare) {
+            // Check if both documents loaded before comparing
+            const beforeColor = new Color(21, 205, 131, 0.4);
+            const afterColor = new Color(255, 73, 73, 0.4);
+            const options = { beforeColor, afterColor };
+            await documentViewer1.startSemanticDiff(documentViewer2, options);
+          }
+        };
+        documentViewer1.addEventListener("documentLoaded", startCompare);
+        documentViewer2.addEventListener("documentLoaded", startCompare);
+        documentViewer1.loadDocument("/pdf/semantic_1.pdf");
+        documentViewer2.loadDocument("/pdf/semantic_2.pdf");
+      });
     });
   }, []);
 
